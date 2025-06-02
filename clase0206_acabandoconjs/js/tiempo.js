f7be8232b328bc87b913816e14ea208f032f4bc9
raulalbiol <raul.albiol@thepower.education>
2025-06-02 19:51:27 +0200

    Clase 02/06/2025

---
+++ clase0206_acabandoconjs/js/tiempo.js
@@ -0,0 +1,20 @@
+let tiempo = document.querySelector("#tiempo");
+let segundos = 0;
+
+// 1s = 1000ms
+/*
+SET INTERVAL (FUNCIÓN, TIEMPO (ms))
+*/
+
+let intervalo = setInterval(function(){
+    segundos++;
+    tiempo.innerHTML = segundos;
+}, 1000);
+
+/* SET TIMEOUT (FUNCIÓN, TIEMPO (ms))
+*/
+
+setTimeout(function(){
+    //clearInterval(intervalo);
+    alert("Se acabo el tiempo");
+},5000);
